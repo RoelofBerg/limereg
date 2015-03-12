15ccc26d7f67317ce082429541c39b32213ad931
Roelof Berg <rberg@berg-solutions.de>
2015-03-12 23:14:42 +0100

    Limereg test working now.

--- tests/test_limereg.c
+++ tests/test_limereg.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int retcode=0;
 
+	//Execute limereg executable and analyze console output
 	printf("Testing limereg executable\n");
-	retcode = system("../exe/limereg --tfile testimg/T_4096.bmp --rfile testimg/R_4096.bmp --nogui "
-	                 "| tee /dev/tty | grep -G 'w = \[5.* deg, 6.*, 126.*\]'");
+	retcode = system("../exe/limereg --tfile testimg/T_512.bmp --rfile testimg/R_512.bmp --nogui "
+	                 "| tee /dev/tty | grep -G 'w = \\[5.* deg, 0.5.*, 14.6.*\\]'");
 
+	//Output and return test result
 	printf("RETVAL: %d\n", retcode);
-	outputTestResult();
+	outputTestResult((0 == retcode) ? true : false);
+	return retcode;
 }
 
 #endif
