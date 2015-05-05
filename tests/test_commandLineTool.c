32ba0883df268490d76390d8e1ae2179321d935a
Roelof Berg <rberg@berg-solutions.de>
2015-05-05 23:24:57 +0200

    Also use lime images in the tests.

--- tests/test_commandLineTool.c
+++ tests/test_commandLineTool.c
@@ -5,6 +5,11 @@
 
 #if HAVE_BOOST==1 && HAVE_BOOST_PROGRAM_OPTIONS_HPP==1 && HAVE_OPENCV==1
 
+int executeShellCommand(const char* command)
+{
+    printf("%s\n", command);
+    return system(command);
+}
 
 int main(void)
 {
@@ -12,10 +17,18 @@ int main(void)
 
 	//Execute limereg executable and analyze console output
 	printf("Test: Command line tool 'limereg'\n");
-	char command[] = "../exe/limereg --tfile " IMGDIR "T_512.bmp --rfile " IMGDIR "R_512.bmp --nogui "
-                   "| tee /dev/tty | grep -G 'w = \\[5.* deg, 0.5.*, 14.6.*\\]'";
-	printf("%s\n", command);
-	retcode = system(command);
+	retcode = executeShellCommand(
+	        "../exe/limereg --tfile " IMGDIR "T_512.bmp --rfile " IMGDIR "R_512.bmp --nogui "
+            "| tee /dev/tty | grep -G 'w = \\[5.* deg, 0.5.*, 14.6.*\\]'"
+	        );
+
+	if(0 == retcode) //continue upon success
+	{
+	    retcode = executeShellCommand(
+	            "../exe/limereg --tfile " IMGDIR "lime1.png --rfile " IMGDIR "lime2.png --nogui --invert"
+	            "| tee /dev/tty | grep -G 'w = \\[-4.* deg, 36.*, -55.*\\]'"
+	            );
+	}
 
 	//Output and return test result
 	outputTestResult((0 == retcode) ? true : false);
